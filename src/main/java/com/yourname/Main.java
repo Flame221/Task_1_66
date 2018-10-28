0255964e4d78742ab48e2ac5b48c90426f6efee8
Dima <thelordser221@gmail.com>
2018-10-28 09:18:24 +0200

    Initial commit

---
+++ src/main/java/com/yourname/Main.java
@@ -0,0 +1,33 @@
+package com.yourname;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c, d;
+        System.out.println("Решение квадратного уровнения ax^2 + bx + c = 0");
+        System.out.println("Введите значение а, b и c");
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        b = s.nextInt();
+        c = s.nextInt();
+        d = b * b - 4 * a * c;
+        if (d > 0) {
+            double x1, x2;
+            x1 = (-1 * b + Math.sqrt(d)) / (2 * a);
+            x2 = (-1 * b - Math.sqrt(d)) / (2 * a);
+            System.out.println("Корни квадратного уровнения равны " + x1 + " и " + x2);
+        } else if (d == 0) {
+            double x;
+            x = (-1 * b / 2 * a);
+            System.out.println("В этом уровнении один корень и он равен" + x);
+        } else {
+            System.out.println("Уровнение не имеет корней");
+        }
+
+
+    }
+
+
+}
+
---
+++ src/main/java/com/yourname/Main1.java
@@ -0,0 +1,32 @@
+package com.yourname;
+
+import java.util.Scanner;
+
+class Main1 {
+    public static void main(String[] args) {
+        int a, b, c;
+        System.out.println("Введите стороны треугольника через пробел");
+
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        b = s.nextInt();
+        c = s.nextInt();
+
+        if (a < b + c && b < a + c && c < a + b) {
+
+            System.out.println("Треугольник с такими сторонами существует");
+        } else {
+            System.out.println("Такого треугольника не существует");
+            return;
+        }
+        if (a == b || a == c || b == c) {
+            System.out.println("Треугольник равнобедренный");
+        }
+        if (b * b == a * a + c * c || a * a == c * c + b * b || c * c == a * a + b * b) {
+            System.out.println("Треугольник прямоугольный");
+        }
+        if (a == b && b == c) {
+            System.out.println("Треугольник равнобедренный");
+        }
+    }
+}
